00626f4d58d800f978b8ecf5bd2a3485a682043e
DESKTOP-KBNVOGG\Youse <yousef01026283799@gmail.com>
2024-03-14 17:34:42 +0200

    -inject properties value

--- spring-project/src/main/java/collegeprojects/springproject/controller/DemoController.java
+++ spring-project/src/main/java/collegeprojects/springproject/controller/DemoController.java
@@ -1,17 +1,26 @@
 package collegeprojects.springproject.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
     @GetMapping("/hello-world")
-    public String HelloWorld(){
+    public String HelloWorld() {
         return "Hello World!";
     }
 
     @GetMapping("/hello-my-world")
-    public String HelloMyFriend(){
+    public String HelloMyFriend() {
         return "Hello my friend";
+    }
+
+    @Value("${developer.name}")
+    private String developerName;
+
+    @GetMapping("/inject-properties")
+    public String properties() {
+        return developerName;
     }
 }
